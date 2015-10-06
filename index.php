a398e618b9197d31d775bccac6a48452c0dc7f7d
PercyODI <pearse.2008@gmail.com>
2015-10-06 02:57:55 +0000

    update

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <?php
           ini_set('display_errors',1);
           ini_set('display_startup_errors',1);
-          error_reporting(-1);
+          error_reporting(E_ALL);
         
           $SERVER = 'us-cdbr-azure-central-a.cloudapp.net';
           $USER = 'bf7f0622e9427e';
@@ -22,7 +22,6 @@
           
           $mylink = new mysqli($SERVER, $USER, $PASS, $DATABASE);
           $_SESSION['mylink'] = $mylink;
-          
           
         ?>  
         
@@ -34,7 +33,7 @@
     </head>
     <body>
         <div class="container">
-            <!--<div class="row">-->
+            <div class="row">
                 <form action="<?=$_SERVER['PHP_SELF']?>" method="POST">
                     <div class="form-group">
                         <div class="radio">
@@ -45,19 +44,19 @@
                         </div>
                         <div class="radio">
                             <label for="city">
-                                <input type="radio" name="fromRadio" value="city" id="city"> 
+                                <input type="radio" name="fromRadio" value="City" id="city"> 
                                 City
                             </label>
                         </div>
                         <div class="radio">
                             <label for="language">
-                                <input type="radio" name="fromRadio" value="language" id="language">
+                                <input type="radio" name="fromRadio" value="countrylanguage" id="language">
                                 Language
                             </label>
                         </div>  
                         <div class="form-group">
                             <label for="search">Search</label>
-                            <input type="text" class="form-control" id="searchText" placeholder="Search...">
+                            <input type="text" class="form-control" name="searchText" id="searchText" placeholder="Search...">
                         </div>
                         <button type="submit" class="btn btn-default">Submit</button>
                     </div>
@@ -65,19 +64,30 @@
                 
                 <?php
                     echo "Hello?";
-                    // echo $_POST['fromRadio'];
+                    echo $_POST['fromRadio'];
+                    echo $_POST['searchText'];
                     // print_r($mylink);
-                    $stmt = $mylink->prepare("SELECT * FROM ? WHERE name=?");
-                    print_r($stmt);
+                    // $stmt = $mylink->prepare("SELECT * FROM city WHERE name='english'");
+                    // print_r($stmt);
                     $results = null;
-                    if($stmt = $mylink->prepare("SELECT * FROM ? WHERE name=?")) {
+                    // $stmt = $mylink->stmt_init();
+                    if($stmt = $mylink->prepare("SELECT CountryCode FROM countrylanguage WHERE language=?")) {
                         echo "<br>In If!<br>";
-                        $stmt->bind_param("ss", $_POST['fromRadio'], $_POST['searchText']);
+                        // $stmt->bind_param("ss", $_POST['fromRadio'], $_POST['searchText']);
+                        $stmt->bind_param("s", $_POST['searchText']);
                         $stmt->execute();
                         $stmt->bind_result($results);
-                        $stmt->fetch();
-                        print_r($results);
+                        while($stmt->fetch()) {
+                            echo "<br>";
+                            print_r($results);
+                        }
                         $stmt->close();
+                    } else {
+                        echo "<br>In Else :(";
+                    }
+                    
+                    if ( false===$stmt ) {
+                      die('prepare() failed: ' . htmlspecialchars($mylink->error));
                     }
                     mysqli_close($mylink);
                 ?>
